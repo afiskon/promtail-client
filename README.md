9fe2754e57debb7a51101e4942e39015ca65ec78
Aleksander Alekseev <mail@eax.me>
2019-02-21 12:35:44 +0300

    Move client example to examples/ directory + minor changes

--- README.md
+++ README.md
@@ -2,72 +2,16 @@
 
 Promtail client library. Promtail is an agent for [Loki](https://github.com/grafana/loki) logging system.
 
-**Warning!** Please note that Loki and Promtail are not
-production ready, at least until [issue #168](https://github.com/grafana/loki/issues/168#issuecomment-455169837) is open.
-
 This library supports both JSON and Protobuf APIs.
 
 Usage example:
 
 ```
-package main
+cd examples/client-example
+go build
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"time"
-	"github.com/afiskon/promtail-client/promtail"
-)
-
-func displayUsage() {
-	fmt.Fprintf(os.Stderr, "Usage: %s proto|json\n", os.Args[0])
-	os.Exit(1)
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		displayUsage()
-	}
-
-	format := os.Args[1]
-	if format != "proto" && format != "json" {
-		displayUsage()
-	}
-
-	conf := promtail.ClientConfig{
-		PushURL:            "http://localhost:3100/api/prom/push",
-		Labels:             "{job=\"bar\"}",
-		BatchWait:          5 * time.Second,
-		BatchEntriesNumber: 10000,
-		SendLevel: 			promtail.INFO,
-		PrintLevel: 		promtail.ERROR,
-	}
-
-	var (
-		loki promtail.Client
-		err error
-	)
-
-	if format == "proto" {
-		loki, err = promtail.NewClientProto(conf)
-	} else {
-		loki, err = promtail.NewClientJson(conf)
-	}
-
-	if err != nil {
-		log.Printf("promtail.NewClient: %s\n", err)
-		os.Exit(1)
-	}
-
-	for i := 1; i < 5; i++ {
-		loki.Debugf("The time is %s, i = %d\n", time.Now().String(), i)
-		loki.Infof("The time is %s, i = %d\n", time.Now().String(), i)
-		loki.Warnf("The time is %s, i = %d\n", time.Now().String(), i)
-		loki.Errorf("The time is %s, i = %d\n", time.Now().String(), i)
-		time.Sleep(1 * time.Second)
-	}
-
-	loki.Shutdown()
-}
+# make sure source-name is unique for every application instance
+# otherwise promtail will reject logs with error:
+# "entry out of order for stream"
+./client-example proto source-name job-name
 ```
---
+++ examples/client-example/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"github.com/afiskon/promtail-client/promtail"
+)
+
+func displayUsage() {
+	fmt.Fprintf(os.Stderr, "Usage: %s proto|json source-name job-name\n", os.Args[0])
+	os.Exit(1)
+}
+
+func displayInvalidName(arg string) {
+	fmt.Fprintf(os.Stderr, "Invalid %s: allowed characters are a-zA-Z0-9_-\n", arg)
+	os.Exit(1)
+}
+
+func nameIsValid(name string) bool {
+	for _, c := range name {
+        if !((c >= 'a' && c <= 'z') ||
+             (c >= 'A' && c <= 'Z') ||
+             (c >= '0' && c <= '9') ||
+             (c == '-') || (c == '_')) {
+            return false
+        }
+    }
+	return true
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		displayUsage()
+	}
+
+	format := os.Args[1]
+	source_name := os.Args[2]
+	job_name := os.Args[3]
+	if format != "proto" && format != "json" {
+		displayUsage()
+	}
+
+	if !nameIsValid(source_name) {
+		displayInvalidName("source-name")
+	}
+
+	if !nameIsValid(job_name) {
+		displayInvalidName("job-name")
+	}
+
+	labels := "{source=\""+source_name+"\",job=\""+job_name+"\"}"
+	conf := promtail.ClientConfig{
+		PushURL:            "http://localhost:3100/api/prom/push",
+		Labels:             labels,
+		BatchWait:          5 * time.Second,
+		BatchEntriesNumber: 10000,
+		SendLevel: 			promtail.INFO,
+		PrintLevel: 		promtail.ERROR,
+	}
+
+	var (
+		loki promtail.Client
+		err error
+	)
+
+	if format == "proto" {
+		loki, err = promtail.NewClientProto(conf)
+	} else {
+		loki, err = promtail.NewClientJson(conf)
+	}
+
+	if err != nil {
+		log.Printf("promtail.NewClient: %s\n", err)
+		os.Exit(1)
+	}
+
+	for i := 1; i < 5; i++ {
+		tstamp := time.Now().String()
+		loki.Debugf("source = %s time = %s, i = %d\n", source_name, tstamp, i)
+		loki.Infof("source = %s, time = %s, i = %d\n", source_name, tstamp, i)
+		loki.Warnf("source = %s, tine = %s, i = %d\n", source_name, tstamp, i)
+		loki.Errorf("source = %s, time = %s, i = %d\n", source_name, tstamp, i)
+		time.Sleep(1 * time.Second)
+	}
+
+	loki.Shutdown()
+}
